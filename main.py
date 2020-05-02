2e54ed27c7d63cbbca66bc47a6038b01a3372cb0
grm4871 <grm4871@rit.edu>
2020-05-02 14:39:52 -0400

    code separation, minor tweaks

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import asyncio
 import jsonsave
 import random
 import webcolors
-
+from guilds import Party
+from guilds import Guild
+from guilds import nextPhase
 
 #emoji library stuff
 emojis = []
@@ -15,253 +17,9 @@ with open('emojis.txt', encoding='utf-8-sig') as f:
     for line in f.readlines():
         emojis.append(line[0])
 
-async def nextPhase(g):
-    guild = client.get_guild(g.server)
-    channel = guild.get_channel(g.electionChannel)
-
-    #primary
-    if g.phase == 0:
-        g.electionMessage = [] #there are now multiple elections (possibly)
-        candidatePairs = {}
-        #build list of candidates / parties
-        for c in g.candidates:
-            for party in g.parties:
-                if c in party.members:
-                    try:
-                        member = guild.get_member(int(c))
-                        if party.name in candidatePairs: candidatePairs[party.name].append(c)
-                        else: candidatePairs[party.name] = [c]
-                    except:
-                        pass
-        #create and send each primary message
-        for party in candidatePairs:
-            c = {}
-            s = "`" + party + '` Primary: \n'
-            for candidate in candidatePairs[party]:
-                #pick an unused emoji
-                while(True):
-                    e = random.choice(list(emojis))
-                    if not e in c.values():
-                        c[candidate] = e
-                        break
-                member = guild.get_member(int(candidate))
-                s += member.mention + ' ' + c[candidate] + '\n'
-            m = await channel.send(s)
-            for cand in c:
-                await m.add_reaction(emoji.emojize(c[cand]))
-            g.electionMessage.append([m.id,c])
-        for party in g.parties:
-            if not party.name in candidatePairs:
-                partyrole = guild.get_role(party.role)
-                try:
-                    await partyrole.delete()
-                except:
-                    pass
-                g.parties.remove(party)
-        if g.candidates == []:
-            await channel.send("No candidates to run primary with!")
-
-    #end primary
-    elif g.phase == 1:
-        newCandidates = []
-        for primary in g.electionMessage:
-            try:
-                msg = await channel.fetch_message(primary[0])
-                results = {}
-                for reaction in msg.reactions:
-                    if str(reaction.emoji) in primary[1].values():
-                        for key in primary[1]:
-                            if primary[1][key] == str(reaction.emoji):
-                                candidate = key
-                        results[candidate] = reaction.count
-                highest = None
-                for candidate in results:
-                    if member_exists(candidate, guild):
-                        if highest == None:
-                            highest = candidate
-                        elif results[highest] < results[candidate]:
-                            highest = candidate
-                        elif results[highest] == results[candidate]:
-                            if random.randint(1,2) == 2:
-                                highest = candidate
-                newCandidates.append(highest)
-                await msg.delete()
-            except:
-                pass
-        g.candidates = newCandidates
-
-        #print winners
-        outputs = []
-        idx = 0
-        num = 0
-        outputs.append("Winning candidates:")
-        for c in newCandidates:
-            if num > 8:
-                idx+=1
-                outputs.append("")
-            try:
-                outputs[idx] += "`" + guild.get_member(int(c)).name + "`\n"
-            except:
-                user = await client.fetch_user(c)
-                outputs[idx] += "`" + user.name + "`\n"
-            num+=1
-        for m in outputs:
-            if m == "Winning candidates:":
-                await channel.send("Primary finished with zero candidates.")
-                await channel.send("The current President will remain in power.")
-            else:
-                await channel.send(m)
-
-    #election
-    elif g.phase == 2:
-        c = {}
-        s = 'General Election: \n'
-        for candidate in g.candidates:
-            try:
-                #pick an unused emoji
-                while(True):
-                    e = random.choice(list(emojis))
-                    if not e in c.values():
-                        c[candidate] = e
-                        break
-                member = guild.get_member(int(candidate))
-                s += member.mention + ' ' + c[candidate] + '\n'
-            except:
-                pass
-        m = await channel.send(s)
-        for cand in c:
-            await m.add_reaction(c[cand])
-        g.electionMessage = [m.id,c]
-
-    #end election
-    elif g.phase == 3:
-        msg = await channel.fetch_message(g.electionMessage[0])
-        results = {}
-        for reaction in msg.reactions:
-            if str(reaction.emoji) in g.electionMessage[1].values():
-                for key in g.electionMessage[1]:
-                    if g.electionMessage[1][key] == str(reaction.emoji):
-                        candidate = key
-                results[candidate] = reaction.count
-        highest = None
-        for candidate in results:
-            if member_exists(candidate, guild):
-                if highest == None:
-                    highest = candidate
-                elif results[highest] < results[candidate]:
-                    highest = candidate
-                elif results[highest] == results[candidate]:
-                    if random.randint(1,2) == 2:
-                        highest = candidate
-        if highest == None:
-            #keep sitting pres
-            await channel.send("A president could not be chosen.")
-            pass
-        else:
-            #change the guard
-            member = guild.get_member(int(highest))
-            if g.current_pres:
-                oldpres = guild.get_member(int(g.current_pres))
-            else:
-                oldpres = None
-            try:
-                presrole = guild.get_role(int(g.presidentRole))
-                await member.add_roles(presrole)
-            except:
-                presrole = await guild.create_role(name="President",color=discord.Color.gold(),hoist=True)
-                g.presidentRole = presrole.id
-                await member.add_roles(presrole)
-            if oldpres:
-                await oldpres.remove_roles(presrole)
-            g.current_pres = highest
-            await channel.send("Your new president is: " + member.mention)
-        await msg.delete()
-        g.candidates = []
-        g.phase = -1
-
-    g.phase += 1
-    global GUILDS
-    save_guilds(GUILDS)
-
-#classes & definitions
-class Guild():
-    def __init__(self, server, parties=[], current_pres=None, current_vice=None, 
-                candidates=[], phase=0, electionMessage=None, settings={"autoelections":False},
-                electionChannel=None, presidentRole=None):
-
-        self.server = int(server)
-        self.parties = []
-        for p in parties:
-            self.parties.append(Party(p[0],p[1],p[2],p[3],p[4]))
-        self.current_pres = current_pres
-        self.current_vice = current_vice
-
-        #election
-        self.candidates = candidates
-        self.phase = phase #0, 1, 2, 3
-        self.electionMessage = electionMessage #message id or array of message ids
-
-        if type(settings) is not dict:
-            settings={"autoelections":False}
-        self.settings = settings
-
-
-        self.electionChannel = electionChannel
-        self.presidentRole = presidentRole
-
-    def nextPhase(self):
-        guild = client.get_guild(self.server)
-        channel = guild.get_channel(self.electionChannel)
-        loop.run_until_complete(nextPhase(this))
-        
-    def noPres():
-        pass
-
-    def addCandidate():
-        pass
-
-    def remCandidate():
-        pass
-
-    def changePartyInfo(name=None, color=None, desc=None):
-        pass
-
-    def output_data(self): #for saving to file
-        data = []
-        data.append(self.server)
-        p = []
-        for party in self.parties:
-            p.append(party.output_data())
-        data.append(p)
-        data.append(self.current_pres)
-        data.append(self.current_vice)
-        data.append(self.candidates)
-        data.append(self.phase)
-        data.append(self.electionMessage)
-        data.append(self.settings)
-        data.append(self.electionChannel)
-        data.append(self.presidentRole)
-        return data
-
-class Party():
-    def __init__(self, name, color, members, role, desc):
-        self.name = name
-        self.color = color
-        self.members = members
-        self.role = role
-        self.desc = ""
-
-    def output_data(self):
-        data = []
-        data.append(self.name)
-        data.append(self.color)
-        data.append(self.members)
-        data.append(self.role)
-        data.append(self.desc)
-        return data
-
 #per-instance bot state
 GUILDS = [] #top-level, i think this is the only level actually (for now)
+MEMBERS = {}
 
 #helper functions
 def fetch_guild(id):
@@ -287,11 +45,11 @@ def server_registered(id):
 
 #save guilds
 def save_guilds(guilds):
-    jsonsave.save_list(guilds, "guilds.txt")
+    jsonsave.save_list(guilds, "data/guilds.txt")
 
 #load guilds
 def load_guilds():
-    loads = jsonsave.load_list("guilds.txt")
+    loads = jsonsave.load_list("data/guilds.txt")
     GUILDS = []
     for guild in loads:
         GUILDS.append(Guild(guild[0],guild[1],guild[2],guild[3],guild[4],
@@ -369,7 +127,8 @@ async def on_message(message):
                         !joinparty - joins a party
                         !parties - lists the parties
                         !runforoffice - adds you to the list of candidates
-                        !candidates - lists all candidates""")
+                        !candidates - lists all candidates
+                        !ownerhelp - lists all server owner commands""")
 
     if server_registered(message.guild.id):# and not (message.author.id == message.guild.owner.id):
         g = server_registered(message.guild.id)
@@ -510,6 +269,16 @@ async def on_message(message):
 
     if message.author.id == message.guild.owner.id:
         g = server_registered(message.guild.id)
+
+        if message.content.startswith('!ownerhelp'):
+            await message.channel.send(
+                """Owner/Debug commands:
+                !forceelection: forces the next phase of the election cycle
+                !resetelection: goes back to the pre-primary phase of the election cycle
+                !electionstate: shows which state of the election cycle this server is in
+                !autoelections: toggles automatic primaries/elections on wednesday and saturday
+                !deleteparty: deletes a party""")
+
         if message.content.startswith('!forceelection'):
             await nextPhase(g)
 
@@ -526,6 +295,23 @@ async def on_message(message):
             else: await message.channel.send("Automatic elections off!")
             save_guilds(GUILDS)
 
+        if message.content.startswith('!deleteparty'):
+            args = message.content.split(' ')
+            if len(args) < 2:
+                await message.channel.send('Usage: !deleteparty partyname')
+            name = message.content[13:]
+            party = None
+            for p in g.parties:
+                if p.name == name:
+                    party = p
+            if party:
+                partyrole = message.guild.get_role(party.role)
+                await partyrole.delete()
+                g.parties.remove(party)
+                await message.channel.send('Party deleted successfully!')
+            else:
+                await message.channel.send('Party not found')
+
 print("started! uwu")
 
 
@@ -538,7 +324,7 @@ async def clock():
         await asyncio.sleep(5)
         global GUILDS
         for guild in GUILDS:
-            if datetime.now().hour > 20 and guild.settings["autoelections"]:
+            if datetime.now().hour > 19 and guild.settings["autoelections"]:
                 if days[guild.phase] == datetime.now().weekday():
                     await nextPhase(guild)
 
@@ -546,4 +332,5 @@ async def clock():
 
 if __name__ == '__main__':
     client.loop.create_task(clock())
-    client.run('')
+    with open("data/token.txt") as f:
+        client.run(f.read())
